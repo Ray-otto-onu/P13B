5d785b2266f08b7977d538795f82fbfc4dd3d8c7
RAY OTTO <rjotto123@gmail.com>
2023-12-07 08:46:06 -0500

    P13B-2

--- P13B/13B.cpp
+++ P13B/13B.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
 #include <string>
-
+#include <iomanip>
 using namespace std;
+
 bool isLeapYear(int year);
+string getMonthName(int month);
+string daysInMonth(int month, int year);
 
 int main() {
-    string input;
+    string inputMonth, inputYear;
     do {
-        cout << "Enter a year or Q to quit: ";
-        cin >> input;
-        if (input == "Q" || input == "q") {
+        cout << "Enter a month and year (MM YYYY) or Q to quit: ";
+        cin >> inputMonth;
+
+        if (inputMonth == "Q" || inputMonth == "q") {
             break;
         }
 
-        int year = stoi(input);
-        if (isLeapYear(year)) {
-            cout << year << " is a leap year." << endl;
+        cin >> inputYear;
+
+        int month = stoi(inputMonth);
+        int year = stoi(inputYear);
+
+        if (month < 1 || month > 12 || year < 1582) {
+            cout << "Invalid input. Month should be in the range [1..12] and year should be >= 1582." << endl;
+            continue;
         }
-        else {
-            cout << year << " is not a leap year." << endl;
-        }
+
+        cout << getMonthName(month) << " " << year << " has " << daysInMonth(month, year) << " days." << endl;
+
     } while (true);
+
     return 0;
 }
 
@@ -30,12 +40,29 @@ bool isLeapYear(int year) {
         return false;
     }
 
-    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-        return true;
-    }
-    else {
-        return false;
-    }
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
 
+string getMonthName(int month) {
+    static const string monthNames[] = { "January", "February", "March", "April", "May", "June",
+                                        "July", "August", "September", "October", "November", "December" };
+    return monthNames[month - 1];
+}
 
+string daysInMonth(int month, int year) {
+    int days = 0;
+
+    switch (month) {
+    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        days = 31;
+        break;
+    case 4: case 6: case 9: case 11:
+        days = 30;
+        break;
+    case 2:
+        days = isLeapYear(year) ? 29 : 28;
+        break;
+    }
+
+    return to_string(days);
+}
